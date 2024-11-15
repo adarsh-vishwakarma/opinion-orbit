014bd3d1ffb0b1a9cfe5ac9588a60db69854c1ad
adarsh-vishwakarma <adarshvishwakarma704@gmail.com>
2024-11-15 20:07:20 +0530

    New app initialized

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-function layout({ children }: { children: ReactNode }) {
-  return (
-    <div className="flex justify-center items-center h-screen">{children}</div>
-  );
-}
-
-export default layout
